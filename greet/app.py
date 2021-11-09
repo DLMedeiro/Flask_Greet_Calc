e56021a5ff58a47a7db7b3aa9959e8a65ed2e3df
DanielleM <dlmedeiro@gmail.com>
2021-11-09 15:11:09 -0800

    modify responses

---
+++ greet/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/welcome")
+def welcome():
+    return "Welcome"
+@app.route("/welcome/home")
+def home():
+    return "Welcome home"
+@app.route("/welcome/back")
+def back():
+    return "Welcome back"
